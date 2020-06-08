23f2e84cb2ae201ffe0e289da9294162a6b8d8ae
hariomkushwaha <hari2018cs048abesit@gmail.com>
2020-06-08 22:54:21 +0530

    Updates

---
+++ precache-manifest.90356410d2b608cd832caa8c15d8e5d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90adad465a65da19994ad917d022164",
+    "url": "/Barber-book/index.html"
+  },
+  {
+    "revision": "5004a77e1101fffb6211",
+    "url": "/Barber-book/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2b7468bc4aadd2ee2b46",
+    "url": "/Barber-book/static/css/main.1b49208f.chunk.css"
+  },
+  {
+    "revision": "5004a77e1101fffb6211",
+    "url": "/Barber-book/static/js/2.bd22ecb2.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/Barber-book/static/js/2.bd22ecb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7468bc4aadd2ee2b46",
+    "url": "/Barber-book/static/js/main.34be6c57.chunk.js"
+  },
+  {
+    "revision": "b8f4531ff06841a1ebd8",
+    "url": "/Barber-book/static/js/runtime-main.741f0102.js"
+  }
+]);
